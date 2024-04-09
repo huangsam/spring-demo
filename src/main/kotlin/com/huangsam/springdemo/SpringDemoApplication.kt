d4efbadaf9ee15061537b97164b413141129f55f
Samuel Huang <samhuang91@gmail.com>
2024-04-09 10:21:54 -0700

    Add hello endpoint to demo application

--- src/main/kotlin/com/huangsam/springdemo/SpringDemoApplication.kt
+++ src/main/kotlin/com/huangsam/springdemo/SpringDemoApplication.kt
@@ -2,9 +2,19 @@ package com.huangsam.springdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
-class SpringDemoApplication
+@RestController
+class SpringDemoApplication {
+    @GetMapping("/hello")
+    fun hello(@RequestParam(value = "name", defaultValue = "Sam") name: String?): String {
+        return String.format("Hello world %s!", name)
+    }
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringDemoApplication>(*args)
