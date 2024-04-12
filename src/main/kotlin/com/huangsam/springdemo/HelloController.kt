862fbd9fc380d8e4f91b84ff3f8b7231d89eb527
Samuel Huang <samhuang91@gmail.com>
2024-04-12 01:40:18 -0700

    Add logging to HelloController

--- src/main/kotlin/com/huangsam/springdemo/HelloController.kt
+++ src/main/kotlin/com/huangsam/springdemo/HelloController.kt
@@ -1,5 +1,7 @@
 package com.huangsam.springdemo
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/hello")
 class HelloController {
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
     @GetMapping
     fun helloName(@RequestParam(value = "name", defaultValue = "Sam") name: String?): String {
+        logger.info("We entered the helloName endpoint")
         return String.format("Hello world %s!", name)
     }
 }
