b81811e1a7c156e909ae8fc0e34a47d1cd2008d7
Samuel Huang <samhuang91@gmail.com>
2024-04-14 06:13:48 -0700

    Add logging to HelloService

--- src/main/kotlin/com/huangsam/springdemo/HelloService.kt
+++ src/main/kotlin/com/huangsam/springdemo/HelloService.kt
@@ -1,10 +1,15 @@
 package com.huangsam.springdemo
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class HelloService {
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
     fun greet(name: String): String {
+        logger.debug("We entered the greet method")
         return "Hello world $name!"
     }
 }
