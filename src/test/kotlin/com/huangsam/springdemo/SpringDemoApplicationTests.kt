8b1c9076f29d7c95b65f25ebc82952e808fe2ddb
Samuel Huang <samhuang91@gmail.com>
2024-04-14 16:03:42 -0700

    Link HTML controller in tests

--- src/test/kotlin/com/huangsam/springdemo/SpringDemoApplicationTests.kt
+++ src/test/kotlin/com/huangsam/springdemo/SpringDemoApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.huangsam.springdemo
 
+import com.huangsam.springdemo.blog.HtmlController
 import com.huangsam.springdemo.hello.HelloController
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
@@ -7,12 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringDemoApplicationTests {
-    @Autowired
-    private lateinit var helloController: HelloController
-
+class SpringDemoApplicationTests(
+    @Autowired private val helloController: HelloController,
+    @Autowired private val htmlController: HtmlController
+) {
     @Test
     fun contextLoads() {
         assertNotNull(helloController)
+        assertNotNull(htmlController)
     }
 }
