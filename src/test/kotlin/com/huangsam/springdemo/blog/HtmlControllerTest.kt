7a3fde6a1faa2698c6f84b4c038ab7d723ecca43
Samuel Huang <samhuang91@gmail.com>
2024-04-14 16:56:34 -0700

    Hook JPA logic up to blog controller

--- src/test/kotlin/com/huangsam/springdemo/blog/HtmlControllerTest.kt
+++ src/test/kotlin/com/huangsam/springdemo/blog/HtmlControllerTest.kt
@@ -1,8 +1,12 @@
 package com.huangsam.springdemo.blog
 
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -13,10 +17,39 @@ import org.springframework.http.HttpStatus
 class HtmlControllerTest(
     @Autowired private val restTemplate: TestRestTemplate,
 ) {
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+    @BeforeAll
+    fun setUp() {
+        logger.info("Set up HTML test")
+    }
+
+    @AfterAll
+    fun tearDown() {
+        logger.info("Tear down HTML test")
+    }
+
     @Test
     fun `Assert blog page title, content and status code`() {
+        logger.info("Assert blog page works")
         val entity = restTemplate.getForEntity<String>("/")
         assertEquals(HttpStatus.OK, entity.statusCode)
-        assertTrue(entity.body!!.contains("<h1>Blog</h1>"))
+        entity.body!!.let {
+            assertTrue(it.contains("<h1>Blog</h1>"))
+            assertTrue(it.contains("Lorem"))
+        }
+    }
+
+    @Test
+    fun `Assert article page title, content and status code`() {
+        logger.info("Assert article page works")
+        val title = "Lorem"
+        val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+        assertEquals(HttpStatus.OK, entity.statusCode)
+        entity.body!!.let {
+            assertTrue(it.contains(title))
+            assertTrue(it.contains("Lorem"))
+            assertTrue(it.contains("dolor sit amet"))
+        }
     }
 }
