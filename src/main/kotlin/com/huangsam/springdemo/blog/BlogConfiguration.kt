2b89c674efca9a632a56c38e79e3d6aa9f1b7a0a
Samuel Huang <samhuang91@gmail.com>
2024-04-14 22:49:54 -0700

    Add logs to BlogConfiguration

--- src/main/kotlin/com/huangsam/springdemo/blog/BlogConfiguration.kt
+++ src/main/kotlin/com/huangsam/springdemo/blog/BlogConfiguration.kt
@@ -1,17 +1,24 @@
 package com.huangsam.springdemo.blog
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class BlogConfiguration {
+    val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
     @Bean
     fun databaseInitializer(
         userRepository: UserRepository,
         articleRepository: ArticleRepository,
     ) = ApplicationRunner {
+        logger.info("Create user John Doe")
         val johnDoe = userRepository.save(User("johnDoe", "John", "Doe"))
+
+        logger.info("Create articles that John Doe is an author of")
         articleRepository.save(
             Article(
                 title = "Lorem",
