c2ff53e68eafcf553836cff25e65a7e62d701e83
Samuel Huang <samhuang91@gmail.com>
2024-04-14 21:28:57 -0700

    Refactor hello package logic

--- src/main/kotlin/com/huangsam/springdemo/hello/HelloController.kt
+++ src/main/kotlin/com/huangsam/springdemo/hello/HelloController.kt
@@ -1,5 +1,6 @@
 package com.huangsam.springdemo.hello
 
+import com.huangsam.springdemo.Routes
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,20 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-const val HELLO_URL = "/hello"
-
 const val DEFAULT_SAM = "Sam"
 
 @RestController
-@RequestMapping(HELLO_URL)
+@RequestMapping(Routes.HELLO)
 class HelloController(private val helloService: HelloService) {
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
     @GetMapping
-    fun helloName(
-        @RequestParam(value = "name", defaultValue = DEFAULT_SAM) name: String
-    ): String {
-        logger.debug("We entered the helloName endpoint")
+    fun helloName(@RequestParam(value = "name", defaultValue = DEFAULT_SAM) name: String): String {
+        logger.debug("We entered ${Routes.HELLO}")
         return helloService.greet(name)
     }
 }
--- src/main/kotlin/com/huangsam/springdemo/hello/HelloService.kt
+++ src/main/kotlin/com/huangsam/springdemo/hello/HelloService.kt
@@ -1,15 +1,10 @@
 package com.huangsam.springdemo.hello
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class HelloService {
-    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
-
     fun greet(name: String): String {
-        logger.debug("We entered the greet method")
         return "Hello world $name!"
     }
 }
