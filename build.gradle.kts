8242d049e7ff0445797a40e3f52b0ec7719cd323
Samuel Huang <samhuang91@gmail.com>
2026-03-01 15:14:02 -0800

    Update kotlin version to 2.3.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.springframework.boot") version "3.5.11"
     id("io.spring.dependency-management") version "1.1.7"
 
-    val kotlinVersion = "2.2.21"
+    val kotlinVersion = "2.3.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
