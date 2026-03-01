4f07fe32ad54addb98b5747179f3535fd4cc6c3f
Samuel Huang <samhuang91@gmail.com>
2026-03-01 15:12:30 -0800

    Update spring boot to latest 3.5 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "3.5.11"
     id("io.spring.dependency-management") version "1.1.7"
 
     val kotlinVersion = "2.2.21"
